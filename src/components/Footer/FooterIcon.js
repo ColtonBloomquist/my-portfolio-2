a6e9a644762eeb07f52d080f01829267ea33f7f8
colton bloomquist <coltonbloomquist@yahoo.com>
2021-12-06 20:13:36 -0700

    updated links

--- src/components/Footer/FooterIcon.js
+++ src/components/Footer/FooterIcon.js
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 function FooterIcon(props) {
   return (
     <li className="footer-icon">
-      <a href={props.url}>
+      <a href={props.url} target="_blank">
         <img src={props.iconImage} alt="footer icon"></img>
       </a>
     </li>
